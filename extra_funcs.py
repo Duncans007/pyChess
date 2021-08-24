b696ed8093fa49276dcddd9a384f8a5ad7f96d6e
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-08-24 01:22:50 -0400

    Update and rename draw_board.py to extra_funcs.py

--- extra_funcs.py
+++ extra_funcs.py
@@ -7,3 +7,15 @@ def draw_board(board):
             x_cord = int(1)
             print("| "+board[str(x_cord)+str(y_cord)]+" | "+board[str(x_cord+1)+str(y_cord)]+" | "+board[str(x_cord+2)+str(y_cord)]+" | "+board[str(x_cord+3)+str(y_cord)]+" | "+board[str(x_cord+4)+str(y_cord)]+" | "+board[str(x_cord+5)+str(y_cord)]+" | "+board[str(x_cord+6)+str(y_cord)]+" | "+board[str(x_cord+7)+str(y_cord)]+" | "+str(y_cord))
     print("^  1 ^  2 ^  3 ^  4 ^  5 ^  6 ^  7 ^  8 ^")
+
+
+def change_turn(turn):
+    if turn == "w":
+        return "b"
+    elif turn == "b":
+        return "w"
+
+
+def strout(x,y):
+    out = str(int(x))+str(int(y))
+    return out
