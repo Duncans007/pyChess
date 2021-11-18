9162fdd110bca2e4d7419706449a0f309be4109e
duncans007 <catking006@mgmail.com>
2021-11-17 22:40:42 -0500

    moved variable declarations to if__name__==main statement for easier import

--- PyChess_Interface.py
+++ PyChess_Interface.py
@@ -8,108 +8,7 @@ from socket_functions import *
 from valid_moves import valid_moves
 from make_move import make_move
 
-color_dict = {
-    "light_sq":(138, 120, 93),
-    "dark_sq":(87, 58, 46),
-    "select_sq":(28, 27, 239)
-}
 
-images = {
-    "wk":'piece_images/king_w.png',
-    "wq":'piece_images/queen_w.png',
-    "wc":'piece_images/rook_w.png',
-    "wh":'piece_images/knight_w.png',
-    "wb":'piece_images/bishop_w.png',
-    "wp":'piece_images/pawn_w.png',
-
-    "bk": 'piece_images/king_b.png',
-    "bq": 'piece_images/queen_b.png',
-    "bc": 'piece_images/rook_b.png',
-    "bh": 'piece_images/knight_b.png',
-    "bb": 'piece_images/bishop_b.png',
-    "bp": 'piece_images/pawn_b.png'
-}
-
-board_dict = {
-    "11": "wc",
-    "12": "wp",
-    "13": "  ",
-    "14": "  ",
-    "15": "  ",
-    "16": "  ",
-    "17": "bp",
-    "18": "bc",
-
-    "21": "wh",
-    "22": "wp",
-    "23": "  ",
-    "24": "  ",
-    "25": "  ",
-    "26": "  ",
-    "27": "bp",
-    "28": "bh",
-
-    "31": "wb",
-    "32": "wp",
-    "33": "  ",
-    "34": "  ",
-    "35": "  ",
-    "36": "  ",
-    "37": "bp",
-    "38": "bb",
-
-    "41": "wq",
-    "42": "wp",
-    "43": "  ",
-    "44": "  ",
-    "45": "  ",
-    "46": "  ",
-    "47": "bp",
-    "48": "bq",
-
-    "51": "wk",
-    "52": "wp",
-    "53": "  ",
-    "54": "  ",
-    "55": "  ",
-    "56": "  ",
-    "57": "bp",
-    "58": "bk",
-
-    "61": "wb",
-    "62": "wp",
-    "63": "  ",
-    "64": "  ",
-    "65": "  ",
-    "66": "  ",
-    "67": "bp",
-    "68": "bb",
-
-    "71": "wh",
-    "72": "wp",
-    "73": "  ",
-    "74": "  ",
-    "75": "  ",
-    "76": "  ",
-    "77": "bp",
-    "78": "bh",
-
-    "81": "wc",
-    "82": "wp",
-    "83": "  ",
-    "84": "  ",
-    "85": "  ",
-    "86": "  ",
-    "87": "bp",
-    "88": "bc",
-}
-
-width, height = 800, 800
-rows, cols = 8, 8
-sq_size = width//cols
-
-running = True
-player_turn = "b"
 
 def draw_board_background(window):
     window.fill(color_dict["light_sq"])
@@ -166,6 +65,109 @@ def click_input():
                 return outp
 
 if __name__ == "__main__":
+    color_dict = {
+        "light_sq": (138, 120, 93),
+        "dark_sq": (87, 58, 46),
+        "select_sq": (28, 27, 239)
+    }
+
+    images = {
+        "wk": 'piece_images/king_w.png',
+        "wq": 'piece_images/queen_w.png',
+        "wc": 'piece_images/rook_w.png',
+        "wh": 'piece_images/knight_w.png',
+        "wb": 'piece_images/bishop_w.png',
+        "wp": 'piece_images/pawn_w.png',
+
+        "bk": 'piece_images/king_b.png',
+        "bq": 'piece_images/queen_b.png',
+        "bc": 'piece_images/rook_b.png',
+        "bh": 'piece_images/knight_b.png',
+        "bb": 'piece_images/bishop_b.png',
+        "bp": 'piece_images/pawn_b.png'
+    }
+
+    board_dict = {
+        "11": "wc",
+        "12": "wp",
+        "13": "  ",
+        "14": "  ",
+        "15": "  ",
+        "16": "  ",
+        "17": "bp",
+        "18": "bc",
+
+        "21": "wh",
+        "22": "wp",
+        "23": "  ",
+        "24": "  ",
+        "25": "  ",
+        "26": "  ",
+        "27": "bp",
+        "28": "bh",
+
+        "31": "wb",
+        "32": "wp",
+        "33": "  ",
+        "34": "  ",
+        "35": "  ",
+        "36": "  ",
+        "37": "bp",
+        "38": "bb",
+
+        "41": "wq",
+        "42": "wp",
+        "43": "  ",
+        "44": "  ",
+        "45": "  ",
+        "46": "  ",
+        "47": "bp",
+        "48": "bq",
+
+        "51": "wk",
+        "52": "wp",
+        "53": "  ",
+        "54": "  ",
+        "55": "  ",
+        "56": "  ",
+        "57": "bp",
+        "58": "bk",
+
+        "61": "wb",
+        "62": "wp",
+        "63": "  ",
+        "64": "  ",
+        "65": "  ",
+        "66": "  ",
+        "67": "bp",
+        "68": "bb",
+
+        "71": "wh",
+        "72": "wp",
+        "73": "  ",
+        "74": "  ",
+        "75": "  ",
+        "76": "  ",
+        "77": "bp",
+        "78": "bh",
+
+        "81": "wc",
+        "82": "wp",
+        "83": "  ",
+        "84": "  ",
+        "85": "  ",
+        "86": "  ",
+        "87": "bp",
+        "88": "bc",
+    }
+
+    width, height = 800, 800
+    rows, cols = 8, 8
+    sq_size = width // cols
+
+    running = True
+    player_turn = "b"
+
     #setup pygame
     pygame.init()
     screen = pygame.display.set_mode((width, height))
